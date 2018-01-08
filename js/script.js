346cd63fea26889bee0ce21c6394107eb207ace9
dsychin <desmond@shiunyean.tk>
2018-01-08 17:40:41 +0000

    added more markers and infowindow

--- js/script.js
+++ js/script.js
@@ -1,13 +1,18 @@
 var viewModel = {
     locations: ko.observableArray([{
-        name: 'Devonshire Fisheries',
-        placeId: 'ChIJ4cpG9LrgeUgRnNCiZBYuFRg'
-    }]),
+            placeId: 'ChIJ4cpG9LrgeUgRnNCiZBYuFRg'
+        },
+        {
+            placeId: 'ChIJX2yJc7DgeUgR4RHis-kpEuo'
+        }
+    ]),
     markers: []
 };
 
+var map;
+
 function initMap() {
-    var map = new google.maps.Map(document.getElementById('map'), {
+    map = new google.maps.Map(document.getElementById('map'), {
         center: {
             lat: 52.772099,
             lng: -1.206166
@@ -17,19 +22,41 @@ function initMap() {
 
     var infowindow = new google.maps.InfoWindow();
     var service = new google.maps.places.PlacesService(map);
-    var markers = [];
 
+    // For each location, find the place details from the place id and
+    // make a marker.
     for (var i = 0; i < viewModel.locations().length; i++) {
         service.getDetails({
             placeId: viewModel.locations()[i].placeId
         }, function (place, status) {
             if (status === google.maps.places.PlacesServiceStatus.OK) {
-                var marker = new google.maps.Marker({
-                    position: place.geometry.location,
-                    map: map
-                });
-                viewModel.markers.push(marker);
+                createMarker(place, infowindow);
+            } else {
+                alert('Error! Problem retrieving place details!');
             }
         })
     }
+}
+
+function createMarker(place, infowindow) {
+    var marker = new google.maps.Marker({
+        position: place.geometry.location,
+        map: map,
+        title: place.name
+    });
+    viewModel.markers.push(marker);
+
+    marker.addListener('click', function () {
+        populateInfoWindow(this, infowindow, place)
+    })
+}
+
+function populateInfoWindow(marker, infowindow, place) {
+    if (infowindow.marker != marker) {
+        infowindow.marker = marker;
+        var content = '<strong>' + place.name + '</strong>';
+        infowindow.setContent(content);
+        infowindow.open(map, marker);
+    }
+
 }
